5a7031459c26f483022ad639de6df86e20df06d0
калинин юра <yura.kalinin2011@yandex.com>
2021-12-24 20:41:01 +0700

    Добавьте файлы проекта.

---
+++ ProjectYK/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProjectYK.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
